6a22ae0b7b51a90a98a466e8f821d1d5445708c0
lilrazlil <nikmoshkin.2002@gmail.com>
2022-07-03 20:52:26 +0700

    with buildx arm64/amd64

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --build-arg ALL="$(cat .git/refs/heads/master)+$(date +%c)" --pull --rm -f "golang-api/Dockerfile" -t go-api:latest "golang-api"
-docker build --build-arg ALL="$(cat .git/refs/heads/master)+$(date +%c)" --pull --rm -f "golang-db/Dockerfile" -t go-db:latest "golang-db"
+docker buildx build --platform linux/arm64/v8,linux/amd64 --build-arg ALL="$(cat .git/refs/heads/master)+$(date +%c)" --pull --rm -f "golang-api/Dockerfile" -t go-api:latest "golang-api"
+docker buildx build --platform linux/arm64/v8,linux/amd64 --build-arg ALL="$(cat .git/refs/heads/master)+$(date +%c)" --pull --rm -f "golang-db/Dockerfile" -t go-db:latest "golang-db"
