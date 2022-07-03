a3bae8c4f7b5d9dd41bbfd15241feae372c7f5c4
lilrazlil <nikmoshkin.2002@gmail.com>
2022-07-03 20:34:54 +0700

    with build.sh ARG

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --pull --rm -f "golang-api/Dockerfile" -t go-api:latest "golang-api"
-docker build --pull --rm -f "golang-db/Dockerfile" -t go-db:latest "golang-db"
+docker build --build-arg ALL="$(cat .git/refs/heads/master)+$(date +%c)" --pull --rm -f "golang-api/Dockerfile" -t go-api:latest "golang-api"
+docker build --build-arg ALL="$(cat .git/refs/heads/master)+$(date +%c)" --pull --rm -f "golang-db/Dockerfile" -t go-db:latest "golang-db"
